3b667d59c71d68223c68624ef699838fb076505f
Artur <ricciantony0@gmail.com>
2023-12-26 20:23:17 +0600

    Создание главного метода

--- Program.cs
+++ Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main()
     {
-    
+        // Ввод массива строк с клавиатуры
+        Console.WriteLine("Введите элементы массива через запятую:");
+        string[] inputArray = Console.ReadLine().Split(',');
+
+        // Формирование нового массива
+        string[] resultArray = FilterArray(inputArray);
+
+        // Вывод результатов
+        Console.WriteLine("Исходный массив:");
+        PrintArray(inputArray);
+
+        Console.WriteLine("Новый массив:");
+        PrintArray(resultArray);
     }
 }
